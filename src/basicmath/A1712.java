68b5fdc4907c555cd7a001db90434a9d1c59978f
bae <baesj11@gmail.com>
2021-08-25 17:22:32 +0900

    A1712
    백준 알고리즘 1712번 문제
    기본 수학

---
+++ src/basicmath/A1712.java
@@ -0,0 +1,27 @@
+package basicmath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A1712 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int rst = -1;
+        if(b<c) {
+            int d = c-b;
+            if(a/d==0) {
+                rst = 1;
+            }else {
+                rst = a/d +1;
+            }
+        }
+        System.out.println(rst);
+        br.close();
+    }
+}
