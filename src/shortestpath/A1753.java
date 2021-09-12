e0ffb63e715b5e7ea02cabd7270040e1a5f0e9ab
bae <baesj11@gmail.com>
2021-09-12 15:34:20 +0900

    A1753
    백준 알고리즘 1753번 문제
    다익스트라
    해결

--- src/shortestpath/A1753.java
+++ src/shortestpath/A1753.java
@@ -5,6 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+class ShortVW implements Comparable<ShortVW> {
+    int v, w;
+
+    public ShortVW(int v, int w) {
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(ShortVW o) {
+        return w - o.w;
+    }
+}
+
 public class A1753 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,8 +26,7 @@ public class A1753 {
         int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(br.readLine());
-        int[][] nd = new int[v+1][v+1];
-        ArrayList<Integer>[] ard = new ArrayList[v+1];
+        ArrayList<ShortVW>[] ard = new ArrayList[v+1];
         for(int i = 1; i <= v; i++) {
             ard[i] = new ArrayList<>();
         }
@@ -22,22 +35,28 @@ public class A1753 {
             int a = Integer.parseInt(st2.nextToken());
             int b = Integer.parseInt(st2.nextToken());
             int c = Integer.parseInt(st2.nextToken());
-            nd[a][b] = c;
-            ard[a].add(b);
+            ard[a].add(new ShortVW(b, c));
         }
+
         int[] ld = new int[v+1];
         Arrays.fill(ld, Integer.MAX_VALUE);
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(k);
+        PriorityQueue<ShortVW> q = new PriorityQueue<>();
+        q.offer(new ShortVW(k, 0));
         ld[k] = 0;
-        int sum = 0;
-        while (!(q.isEmpty())) {
-            Integer p = q.poll();
-            sum++;
-            for(int g : ard[p]) {
-                if(ld[g] > nd[p][g] + ld[p]) {
-                    q.offer(g);
-                    ld[g] = Math.min(nd[p][g] + ld[p], ld[g]);
+        while(!q.isEmpty()) {
+            ShortVW p = q.poll();
+
+            int ver = p.v;
+            int wei = p.w;
+            if(ld[ver] < wei) {
+                continue;
+            }
+            for(int i = 0; i < ard[ver].size(); i++) {
+                int ver2 = ard[ver].get(i).v;
+                int wei2 = ard[ver].get(i).w + wei;
+                if(ld[ver2] > wei2) {
+                    ld[ver2] = wei2;
+                    q.add(new ShortVW(ver2, wei2));
                 }
             }
         }
@@ -48,8 +67,6 @@ public class A1753 {
                 System.out.println(ld[i]);
             }
         }
-        System.out.println("---");
-        System.out.println(sum);
         br.close();
     }
 }
