f29e27eff3526148aa8aa8adb0a2877030e906e5
bae <baesj11@gmail.com>
2021-09-24 17:00:10 +0900

    dp
    재업로드

--- src/dynamicprogramming/A13699.java
+++ src/dynamicprogramming/A13699.java
@@ -1,2 +1,21 @@
-package dynamicprogramming;public class A13699 {
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A13699 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n+1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        System.out.println(dp[n]);
+        br.close();
+    }
 }
--- src/dynamicprogramming/A17626.java
+++ src/dynamicprogramming/A17626.java
@@ -1,2 +1,25 @@
-package dynamicprogramming;public class A17626 {
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A17626 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        int min;
+        for (int i = 2; i <= n; i++) {
+            min = Integer.MAX_VALUE;
+
+            for (int j = 1; j*j <=i; j++) {
+                int p = i-j*j;
+                min = Math.min(min, dp[p]);
+            }
+            dp[i] = min +1;
+        }
+        System.out.println(dp[n]);
+    }
 }
