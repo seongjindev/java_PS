bdb9e8d0763c39a198f1aa0e10c4b2f0a73e5bbb
bae <baesj11@gmail.com>
2021-10-01 21:09:19 +0900

    A5545
    백준 알고리즘 5545번 문제
    greedy 문제

---
+++ src/greedy/A5545.java
@@ -0,0 +1,38 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class A5545 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        Integer[] bArr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            bArr[i] = Integer.parseInt(br.readLine());
+        }
+        int price = a;
+        int cal = c;
+        int rst = cal/price;
+        Arrays.sort(bArr, Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            if ((cal+bArr[i])/(price+b)>rst) {
+                cal += bArr[i];
+                price += b;
+                rst = cal/price;
+            }else {
+                break;
+            }
+        }
+        System.out.println(rst);
+        br.close();
+    }
+}
