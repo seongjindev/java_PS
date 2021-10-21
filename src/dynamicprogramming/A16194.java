c3b41461aaa89dacab75b65da3368dd5c5fb3700
bae <baesj11@gmail.com>
2021-10-21 20:19:19 +0900

    1문제 풀이
    A16194 - dp

---
+++ src/dynamicprogramming/A16194.java
@@ -0,0 +1,27 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A16194 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i] = arr[i];
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j] + arr[j]);
+            }
+        }
+        System.out.println(dp[n]);
+        br.close();
+    }
+}
