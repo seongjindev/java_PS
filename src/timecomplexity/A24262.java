a4ddeb1acad6e80413551b16dea14ad9fa88b117
SJ <baesj11@gmail.com>
2023-03-17 23:40:20 +0900

    3문제 풀이
    A24262 - timecomplexity
    A24263 - timecomplexity
    A24264 - timecomplexity

---
+++ src/timecomplexity/A24262.java
@@ -0,0 +1,13 @@
+package timecomplexity;
+
+import java.util.Scanner;
+
+public class A24262 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(1);
+        System.out.println(0);
+        sc.close();
+    }
+}
---
+++ src/timecomplexity/A24264.java
@@ -0,0 +1,12 @@
+package timecomplexity;
+
+import java.util.Scanner;
+
+public class A24264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long n = sc.nextLong();
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
