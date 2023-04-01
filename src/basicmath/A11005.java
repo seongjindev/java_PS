21ee6b9c187ea09f5eba6ca0d61d8e986013d28e
SJ <baesj11@gmail.com>
2023-04-02 00:20:25 +0900

    다시 풀이
    A11005 - basicmath

--- src/basicmath/A11005.java
+++ src/basicmath/A11005.java
@@ -16,6 +16,38 @@ import java.util.StringTokenizer;
 //    }
 //}
 
+//public class A11005 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int b = Integer.parseInt(st.nextToken());
+//        StringBuilder sb = new StringBuilder();
+//        while (true) {
+//            int remain = n % b;
+//            n = (n - remain) / b;
+//            char ch = 0;
+//            if (remain > 9) {
+//                ch = (char)(remain + 55);
+//                sb.insert(0, ch);
+//            } else {
+//                sb.insert(0, remain);
+//            }
+//            if (n < b) {
+//                if (n > 9) {
+//                    ch = (char)(n + 55);
+//                    sb.insert(0, ch);
+//                } else if (n > 0){
+//                    sb.insert(0, n);
+//                }
+//                break;
+//            }
+//        }
+//        System.out.println(sb);
+//        br.close();
+//    }
+//}
+
 public class A11005 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,27 +55,17 @@ public class A11005 {
         int n = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
-        while (true) {
+        while (n > 0) {
             int remain = n % b;
             n = (n - remain) / b;
-            char ch = 0;
             if (remain > 9) {
-                ch = (char)(remain + 55);
-                sb.insert(0, ch);
+                sb.insert(0, (char)(remain + 55));
             } else {
                 sb.insert(0, remain);
-            }
-            if (n < b) {
-                if (n > 9) {
-                    ch = (char)(n + 55);
-                    sb.insert(0, ch);
-                } else {
-                    sb.insert(0, n);
-                }
-                break;
             }
         }
         System.out.println(sb);
         br.close();
     }
 }
+
