eeff77f5a746f2faaa4b1d03a0fabaad143fc047
SJ <baesj11@gmail.com>
2023-04-02 23:12:26 +0900

    1문제 풀이
    A15439 - numberandcombinatorics

---
+++ src/numberandcombinatorics/A15439.java
@@ -0,0 +1,12 @@
+package numberandcombinatorics;
+
+import java.util.Scanner;
+
+public class A15439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((n-1) * n);
+        sc.close();
+    }
+}
