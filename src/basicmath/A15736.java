262a3947636fd09865bd91d7b78de6bea57c8b9b
SJ <baesj11@gmail.com>
2023-05-30 23:47:40 +0900

    1문제 풀이
    A15736 - basicmath

---
+++ src/basicmath/A15736.java
@@ -0,0 +1,18 @@
+package basicmath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A15736 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 1; i * i <= n; i++) {
+            cnt++;
+        }
+        System.out.println(cnt);
+        br.close();
+    }
+}
