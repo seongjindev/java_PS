afed622f42410dbce90e2b855a1a685f4074e1fd
SJ <baesj11@gmail.com>
2023-08-03 23:22:55 +0900

    1문제 풀이
    A11441 - prefixsumps

---
+++ src/prefixsumps/A11441.java
@@ -0,0 +1,28 @@
+package prefixsumps;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A11441 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(arr[b] - arr[a - 1]).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
