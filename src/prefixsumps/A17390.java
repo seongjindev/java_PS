a84270f6644d19a8fe4d93b23ea2f1ad479519f3
SJ <baesj11@gmail.com>
2023-08-04 23:30:38 +0900

    1문제 풀이
    A17390 - prefixsumps

---
+++ src/prefixsumps/A17390.java
@@ -0,0 +1,34 @@
+package prefixsumps;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class A17390 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i];
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            sb.append(arr[r] - arr[l - 1]).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
