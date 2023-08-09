11a1806b3ca6e7ed2e24fe2bdb1b773f9f3b67b4
SJ <baesj11@gmail.com>
2023-08-09 23:44:51 +0900

    1문제 풀이
    A2435 - prefixsumps

---
+++ src/prefixsumps/A2435.java
@@ -0,0 +1,26 @@
+package prefixsumps;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A2435 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        int max = -10001;
+        for (int i = k; i <= n; i++) {
+            max = Math.max(max, arr[i] - arr[i - k]);
+        }
+        System.out.println(max);
+        br.close();
+    }
+}
