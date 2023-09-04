b576ef3e10b1ffe1d4e28e1ef76a4472ecedd459
SJ <baesj11@gmail.com>
2023-09-04 23:59:35 +0900

    1문제 풀이
    A11568 - dp

---
+++ src/dynamicprogramming/A11568.java
@@ -0,0 +1,31 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A11568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[n];
+        int rst = -1;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            rst = Math.max(rst, dp[i]);
+        }
+        System.out.println(rst);
+        br.close();
+    }
+}
